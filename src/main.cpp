163a3d2cac85ffd1bbd612322cc6f29139c6fa4e
TSprech <trey.sprecher@gmail.com>
2021-12-07 07:59:27 -0500

    Simple Flatbuffer Integration
    Added support for Flatbuffer read and write support for saving settings and similar. Note a small change had to be made to the flatbuffers base.h file to remove Arduino tags as standard library is fully supported in PIO.

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,21 @@
+#include <algorithm>
 #include <array>
 #include <cstdint>
+#include <vector>
 
+#include "HeaterSettings_generated.h"
 #include "MicroMod_Pins_Teensy_Processor.hpp"
 #include "SdFat.h"
+#include "flatbuffers/flatbuffers.h"
 #include "sdios.h"  // Using fstream for ease of use as well as the vast space of the Teensy processor
 // Seperator for Arduino header
 #include <Arduino.h>
-
-// // void setup() {
-// //   pinMode(spi_sck, OUTPUT);
-// // }
-
-// // void loop() {
-// //   digitalWrite(spi_sck, HIGH);
-// //   delay(250);
-// //   digitalWrite(spi_sck, LOW);
-// //   delay(250);
-// // }
 
 #define SERIALOUT Serial
 
 const auto sd_cs_pin = g_4;  // d_1 for single, g_4 for double
 
-const uint32_t buffer_size = 100;  // 32 KB buffer
+const uint32_t buffer_size = 200;  // 200B buffer
 std::array<char, buffer_size> sd_buffer = {0};
 
 SdFat sd_card;
@@ -31,21 +24,33 @@ void setup() {
   SERIALOUT.begin(9600);
   while (!SERIALOUT)
     ;
-  if (!sd_card.begin(sd_cs_pin, SD_SCK_MHZ(50)))
+  if (!sd_card.begin(sd_cs_pin, SD_SCK_MHZ(50))) // Start the SD card and increase communication speed to 50MHz (decrease if errors)
     SERIALOUT.print("Error setting up card\n");
 
-  ifstream input_data("/DIR/TEST.TXT");
-  SERIALOUT.printf("File open successfully\n");
+  flatbuffers::FlatBufferBuilder builder(40);
+  auto output_buffer = HeaterSettings::CreateHeaterSettings(builder, 130, true); // Create a new heater settings with the builder
+  builder.Finish(output_buffer);
+  auto buff_ptr = builder.GetBufferPointer(); // Get a pointer to access the Flatbuffer data
 
-  input_data.getline(sd_buffer.data(), sd_buffer.size(), '\n');
-  SERIALOUT.write(sd_buffer.data(), 20);
-
-  ofstream output_data("NEW.TXT");
-  if (output_data.is_open()) {
-    output_data << "Arduino wrote this!\n";
-    output_data.close();
-    SERIALOUT.print("Write success!\n");
+  ofstream output_data("HeaterSettings.txt"); // Open or create a new file
+  if (output_data.is_open()) { // Check that it is open
+    for (size_t offset = 0; offset < builder.GetSize(); ++offset) { // Go through each byte of the builder's buffer that has relevent data
+      auto current_value = *(buff_ptr + offset); // Offset the point appropriately to access the next byte
+      SERIALOUT.print(current_value, HEX); // Display the new byte
+      SERIALOUT.print(' ');
+      output_data << current_value; // Write the byte to the file
+    }
   }
+  output_data << '\n'; // TODO: Use EOF?
+  output_data.close(); // Finish writing
+  SERIALOUT.print("\n");
+
+  ifstream input_buffer("HeaterSettings.txt"); // Open the created file
+  // TODO: Related to EOF, see if that can be used to avoid having to insert newline
+  input_buffer.getline(sd_buffer.data(), sd_buffer.size(), '\n'); // Pull the Flatbuffer raw data from the file
+  auto input_data = HeaterSettings::GetHeaterSettings(sd_buffer.data()); // Take the raw data and make it accessible as heater setting data
+  SERIALOUT.printf("\nSet point: %i", input_data->set_point()); // Print the setpoint (should be 130)
+  SERIALOUT.printf("\nLights: %s", input_data->lights() ? "True" : "False"); // Print the boolean lights (should be true)
 }
 
 void loop() {
